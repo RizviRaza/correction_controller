a47550e65e73608e6a3d4fcde0de735589651d66
Raza Rizvi <smrazarizvi96@gmail.com>
2025-05-23 16:33:04 -0400

    keyboard image press added

--- correction_controller/query_image_pub.py
+++ correction_controller/query_image_pub.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
-from rclpy.qos import QoSProfile, ReliabilityPolicy
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 import sys
 import select
 import termios
@@ -10,6 +10,13 @@ import tty
 class QueryImagePublisher(Node):
     def __init__(self):
         super().__init__('query_image_publisher')
+
+        reliable_qos = QoSProfile(
+            reliability=ReliabilityPolicy.RELIABLE,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=10
+        )
 
         # Subscribes to the compressed image topic
         self.image_sub = self.create_subscription(
@@ -22,8 +29,8 @@ class QueryImagePublisher(Node):
         # Publisher to send latest image when spacebar is pressed
         self.image_pub = self.create_publisher(
             CompressedImage,
-            '/latest_image/compressed',
-            10
+            '/query_image_rgb',
+            reliable_qos
         )
 
         self.latest_image = None
